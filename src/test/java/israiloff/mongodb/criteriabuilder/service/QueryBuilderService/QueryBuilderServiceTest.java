99168d06669affa1ded1abebbf96f064a6ed5b49
Israiloff <stardustindark@gmail.com>
2019-10-05 19:13:07 +0500

    added: tests;
    Signed-off-by: Israiloff <stardustindark@gmail.com>

---
+++ src/test/java/israiloff/mongodb/criteriabuilder/service/QueryBuilderService/QueryBuilderServiceTest.java
@@ -0,0 +1,23 @@
+package israiloff.mongodb.criteriabuilder.service.QueryBuilderService;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class QueryBuilderServiceTest {
+
+    @org.junit.Before
+    public void setUp() {
+        //todo - implement
+    }
+
+    @org.junit.After
+    public void tearDown() {
+        //todo - implement
+    }
+
+    @org.junit.Test
+    public void criteriaQueryBuilder() {
+        //todo - implement
+    }
+}
